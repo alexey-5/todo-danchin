cf3ba059d47d44ab4d3ba3fc909b7e52cd236a7f
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2023-01-07 17:09:14 +0300

    второй

--- src/App.js
+++ src/App.js
@@ -2,29 +2,48 @@ import { useState } from 'react';
 import './app.css'
 
 function App() {
+  const [addItem, setAddItem] = useState('')
+
   const [list, setList] = useState([
     { id: 1, name: 'Планирование', done: false },
     { id: 2, name: 'Вёрстка', done: false },
     { id: 3, name: 'Разметка НТМЛ', done: false },
     { id: 4, name: 'Тестирование', done: false },
-  ])
-  return (
-    <div>
-      <input type='text' />
-      <button type='button'>Добавить</button>
-      <div className='box'>
-      <ul className='list'>
-        {
-          list.map((el) => <li>
-            <button type='button'>Удалить</button>
-            <button type='button'>Сделано</button>
-            <span>{el.name}</span>
-          </li>)
-        }
-      </ul>
-      </div>
-    </div>
-  );
-}
+  ]);
+  
+  const done = (id) => {
+    setList(list.map((item) => {
+      if (item.id === id) return { ...item, done: !item.done }
+      return item
+    }))};
 
-export default App;
+  const del = (id) => {
+    setList(list.filter((item)=> item.id !== id ))
+  }
+
+  const addCase = () => {
+    if(addItem !== '')
+     {setList ([...list, {id: 1+Math.max(0, ...list.map((el)=>el.id)), name: addItem, done: false}])
+     setAddItem('')}
+  }
+  console.log(list)
+    return (
+      <div>
+        <input type='text' value={addItem} onChange={e=>setAddItem(e.target.value)}/>
+        <button type='button' onClick={addCase}>Добавить</button>
+        <div className='box'>
+          <ul className='list'>
+            {
+              list.map((el) => <li key={el.id}>
+                <button type='button' onClick={() => del(el.id)}>Удалить</button>
+                <button type='button' onClick={() => done(el.id)}>Сделано</button>
+                <span className={el.done ? "done" : ""}>{el.name}</span>
+              </li>)
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+  export default App;
